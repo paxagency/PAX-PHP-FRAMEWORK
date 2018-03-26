5b77c2366adc3d07ade34bd277522ebb3954df26
Albert Kiteck <albert@paxagency.com>
2018-03-26 17:06:55 -0500

    update
    update

--- app/php/app/classes/example.php
+++ app/php/app/classes/example.php
@@ -1,7 +1,7 @@
 <?php
 class example {
     public function __construct() {}
-    public function get_string($post=[],$get=[]) {
+    public function get_string($get=[],$post=[]) {
         return "Example Class Called";
     }
 }
