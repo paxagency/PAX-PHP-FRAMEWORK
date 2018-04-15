976e64f4e32ed2128e5363cfa327ecc7ba206aa6
Albert Kiteck <albert@paxagency.com>
2018-04-15 14:00:14 -0500

    Index Logo
    Index Logo

--- app/php/pages/index.php
+++ app/php/pages/index.php
@@ -1,2 +1,5 @@
-<h1>PAX PHP Framework</h1>
-<p><button onclick='window.location.href = "http://docs.paxagency.com/php";'>Click Here to read PAX PHP Docs</button></p>
+<h1 style='text-align:center;font-family:"Helvetica Neue","Helvetica",arial; font-weight:200;'>
+    <span style='font-size:2em;font-weight:900;'>PAX</span> <br />
+    PHP Framework<br />
+    <button onclick='window.location.href = "http://docs.paxagency.com/php";'>Click Here to read PAX PHP Docs</button>
+</h1>
