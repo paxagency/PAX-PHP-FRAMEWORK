795d5c8f27e5b0390ca06a1cd0571dd9053824a1
Albert Kiteck <albert@paxagency.com>
2019-02-07 10:38:58 -0600

    Crud Example
    Crud Example

--- app/php/app/classes/crud.php
+++ app/php/app/classes/crud.php
@@ -3,30 +3,23 @@ class crud {
     public function __construct() {}
     public function save($get=[],$post=[]) {
         if(!$this->auth($get)) return 0;
-        return $this->elastic->save($get[0],$post);
+        return $this->db->save($get[0],$post);
     }
     public function update($get=[],$post=[]) {
         if(!$this->auth($get)) return 0;
-        return $this->elastic->update($get[0],$get[1],$post);
-    }
-    public function bulk($get=[],$post=[]) {
-        foreach($post['data'] as $k=>$p) {
-            if(!isset($p['_action']) || !isset($p['type'])) return 0;
-            $post['data'][$k]['_index'] = $this->elastic->index;
-        }
-        return $this->elastic->bulk($post['data']);
+        return $this->db->update($get[0],$get[1],$post);
     }
     public function delete($get=[],$post=[]) {
         if(!$this->auth($get)) return 0;
-        return $this->elastic->delete($get[0]);
+        return $this->db->delete($get[0],$get[1]);
     }
     public function count($get=[],$post=[]) {
         if(!$this->auth($get)) return 0;
-        return $this->elastic->count($get[0],$post);
+        return $this->db->count($get[0],$post);
     }
     public function get($get=[],$post=[]){
         if(!$this->auth($get)) return ['error'=>'No Access'];
-        return $this->elastic->get($get[0],$get[1]);
+        return $this->db->get($get[0],$get[1]);
     }
     public function search($get=[],$post=[]) {
         if(!$this->auth($get)) return ['hits'=>[],'count'=>0,'error'=>'No Access'];
@@ -35,7 +28,7 @@ class crud {
         $page = $get[2] ?? 0;
         $sort = $get[3] ?? 'asc';
         $order = $get[4] ?? 'id';
-        return $this->elastic->search($type,$post,$max,$page,$order,$sort);
+        return $this->db->search($type,$post,$max,$page,$sort,$order);
     }
     public function auth($get=[]){
         //WRITE CUSTOM AUTH FUNCTION
