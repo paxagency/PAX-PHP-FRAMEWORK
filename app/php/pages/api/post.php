4b75839a83831131426e8280361763a0ca3d4b94
Albert Kiteck <albert@paxagency.com>
2019-04-05 11:01:36 -0500

    Access to previous instanciation
    Access to previous instanciation

--- app/php/pages/api/post.php
+++ app/php/pages/api/post.php
@@ -5,7 +5,7 @@ $function = $_URL[3];
 if(!isset($function)) header('location:'.$_SERVER['HTTP_REFERER']);
 array_splice($_URL,0,4);
 $_URL = array_values($_URL);
-$app = new app([$path]);
+$app->load([$path]);
 $call = $app->$path->$function($_URL,$_POST);
 $url = (isset($call['url'])) ? $call['url'] : $_SERVER['HTTP_REFERER'];
 header('location:'.$url);
